22c57e46948fbb73f3981ec8687280e02934d98c
claudehotline <claudehotline@hotmail.com>
2023-08-16 23:01:42 +0800

    fix: minor bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteMockServe } from 'vite-plugin-mock'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  //获取各种环境下的对应的变量
+  let env = loadEnv(mode, process.cwd());
   return {
     plugins: [
       vue(),
@@ -31,5 +33,14 @@ export default defineConfig(({ command }) => {
         },
       },
     },
+    //代理跨越
+    server:{
+      proxy:{
+        [env.VITE_APP_BASE_API]:{
+          target: env.VITE_SERVE,
+          changeOrigin: true,
+        }
+      }
+    }
   }
 })
