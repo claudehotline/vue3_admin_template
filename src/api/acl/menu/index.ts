e24195f91e91663beade6010f2b935aaa3345292
Administrator <admin@example.com>
2023-08-29 18:57:53 +0800

    feat: add permission page

--- src/api/acl/menu/index.ts
+++ src/api/acl/menu/index.ts
@@ -1,10 +1,11 @@
-import request from '@/utils/request';
-import { PermissionResponseData } from './type';
+import request from '@/utils/request'
+import { PermissionResponseData } from './type'
 //枚举地址
-enum API{
-    //获取全部菜单与按钮的标识数据
-    ALLPERMISSION_URL='/admin/acl/permission',
-    //
+enum API {
+  //获取全部菜单与按钮的标识数据
+  ALLPERMISSION_URL = '/admin/acl/permission',
+  //
 }
 //获取菜单数据
-export const reqAllPermission = () => request.get<any, PermissionResponseData>(API.ALLPERMISSION_URL);
+export const reqAllPermission = () =>
+  request.get<any, PermissionResponseData>(API.ALLPERMISSION_URL)
--- src/views/acl/permission/index.vue
+++ src/views/acl/permission/index.vue
@@ -1,18 +1,38 @@
 <template>
   <el-table
     :data="permissionArr"
-    style="width:100%; margin-bottom: 20px"
+    style="width: 100%; margin-bottom: 20px"
     row-key="id"
     border
   >
-    <el-table-column prop="name" label="名称"/>
-    <el-table-column prop="code" label="权限值"/>
-    <el-table-column prop="updateTime" label="修改时间"/>
+    <el-table-column prop="name" label="名称" />
+    <el-table-column prop="code" label="权限值" />
+    <el-table-column prop="updateTime" label="修改时间" />
     <el-table-column label="操作">
-      <template #="{row, $index}">
-        <el-button type="primary" @click="addPermission" size="small" :disabled="row.level==4?true:false">{{ row.level==3?'添加功能':'添加菜单' }}</el-button>
-        <el-button type="primary" @click="updatePermission(row)" size="small" :disabled="row.level==1?true:false">编辑</el-button>
-        <el-button type="primary" size="small" :disabled="row.level==1?true:false">删除</el-button>
+      <template #="{ row, $index }">
+        <el-button
+          type="primary"
+          @click="addPermission"
+          size="small"
+          :disabled="row.level == 4 ? true : false"
+        >
+          {{ row.level == 3 ? '添加功能' : '添加菜单' }}
+        </el-button>
+        <el-button
+          type="primary"
+          @click="updatePermission(row)"
+          size="small"
+          :disabled="row.level == 1 ? true : false"
+        >
+          编辑
+        </el-button>
+        <el-button
+          type="primary"
+          size="small"
+          :disabled="row.level == 1 ? true : false"
+        >
+          删除
+        </el-button>
       </template>
     </el-table-column>
   </el-table>
@@ -28,8 +48,8 @@
     </el-form>
     <template #footer>
       <span class="dialog-footer">
-        <el-button @click="dialogVisible=false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible=false">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
           确定
         </el-button>
       </span>
@@ -38,32 +58,36 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue'
 //引入获取菜单请求API
-import { reqAllPermission } from '@/api/acl/menu';
+import { reqAllPermission } from '@/api/acl/menu'
 //返回数据的ts类型
-import type { PermissionResponseData, PermissionList, Permission } from '@/api/acl/menu/type';
+import type {
+  PermissionResponseData,
+  PermissionList,
+  Permission,
+} from '@/api/acl/menu/type'
 
-let permissionArr = ref<PermissionList>([]);
-let dialogVisible = ref<boolean>(false);
+let permissionArr = ref<PermissionList>([])
+let dialogVisible = ref<boolean>(false)
 
 onMounted(() => {
-  getHasPermission();
+  getHasPermission()
 })
 
-const getHasPermission = async() => {
-  let result:PermissionResponseData = await reqAllPermission();
-  if(result.code==200){
-    permissionArr.value = result.data;
+const getHasPermission = async () => {
+  let result: PermissionResponseData = await reqAllPermission()
+  if (result.code == 200) {
+    permissionArr.value = result.data
   }
 }
 
 const addPermission = () => {
-  dialogVisible.value = true;
+  dialogVisible.value = true
 }
 
-const updatePermission = (row:Permission)=>{
-  dialogVisible.value = true;
+const updatePermission = (row: Permission) => {
+  dialogVisible.value = true
 }
 </script>
 
